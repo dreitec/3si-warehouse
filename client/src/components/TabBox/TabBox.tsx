42864891ed7b1209a16be1c9d7b624ee4d6d2e3f
Steven Herzog <herzogdev@gmail.com>
2021-10-09 19:45:26 +0500

    make tabs responsive

--- client/src/components/TabBox/TabBox.tsx
+++ client/src/components/TabBox/TabBox.tsx
@@ -7,14 +7,23 @@ const StyledContainer = styled("div")(({ theme }) => ({
 
 const StyledBoxItem = styled(Grid)(({ theme }) => {
   return {
-    color: theme.palette.primary.main,
-    border: `4px solid ${theme.palette.primary.main}`,
+    color: "#376EFF",
+    border: `4px solid #376EFF`,
     textAlign: "center",
     padding: theme.spacing(4),
     "& > svg": {
       fontSize: "4rem",
     },
     backgroundColor: "white",
+    [theme.breakpoints.down("sm")]: {
+      border: `3px solid #376EFF`,
+      fontSize: "2rem",
+      margin: theme.spacing(2),
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1rem",
+      margin: theme.spacing(1),
+    },
   };
 });
 
@@ -22,6 +31,12 @@ const StyledHeading = styled(Typography)(() => ({
   width: "100%",
 }));
 
+const SytledGrid = styled(Grid)(({ theme }) => ({
+  justifyContent: "space-between",
+  [theme.breakpoints.down("sm")]: {
+    justifyContent: "center",
+  },
+}));
 interface Props {
   active?: boolean;
 }
@@ -29,28 +44,28 @@ interface Props {
 const TabBox = (props: Props) => {
   return (
     <StyledContainer>
-      <Grid container justifyContent="space-between">
-        <StyledBoxItem item xs={4} md={2}>
+      <SytledGrid container>
+        <StyledBoxItem item xs={12} sm={5} md={2}>
           <PeopleOutlineIcon fontSize="large" />
           <StyledHeading variant="h6">Eligibility</StyledHeading>
         </StyledBoxItem>
-        <StyledBoxItem item xs={4} md={2}>
+        <StyledBoxItem item xs={12} sm={5} md={2}>
           <PeopleOutlineIcon fontSize="large" />
           <StyledHeading variant="h6">Service</StyledHeading>
         </StyledBoxItem>
-        <StyledBoxItem item xs={4} md={2}>
+        <StyledBoxItem item xs={12} sm={5} md={2}>
           <PeopleOutlineIcon fontSize="large" />
           <StyledHeading variant="h6">Provider</StyledHeading>
         </StyledBoxItem>
-        <StyledBoxItem item xs={4} md={2}>
+        <StyledBoxItem item xs={12} sm={5} md={2}>
           <PeopleOutlineIcon fontSize="large" />
           <StyledHeading variant="h6">Gaps</StyledHeading>
         </StyledBoxItem>
-        <StyledBoxItem item xs={4} md={2}>
+        <StyledBoxItem item xs={12} sm={5} md={2}>
           <PeopleOutlineIcon fontSize="large" />
           <StyledHeading variant="h6">Export</StyledHeading>
         </StyledBoxItem>
-      </Grid>
+      </SytledGrid>
     </StyledContainer>
   );
 };
