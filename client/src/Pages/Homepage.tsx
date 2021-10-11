69ad6c7ff73160882dd682308a64598eaefe0584
Steven Herzog <herzogdev@gmail.com>
2021-10-11 20:02:34 +0500

    add desciption with icon and button

--- client/src/Pages/Homepage.tsx
+++ client/src/Pages/Homepage.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import { Description, TabBox } from "../components";
+import {
+  PeopleOutline as PeopleOutlineIcon,
+  PieChart as PieChartIcon,
+  Map as MapIcon,
+  Height as HeightIcon,
+  SaveAlt as SaveAltIcon,
+} from "@mui/icons-material";
+
 interface Props {}
 
 const Homepage = (props: Props) => {
@@ -20,6 +28,25 @@ const Homepage = (props: Props) => {
       </Description>
 
       <TabBox />
+
+      <Description
+        heading="FIRST GLIMPSE INTO YOUR DATA WAREHOUSE"
+        Icon={PeopleOutlineIcon}
+        button={true}
+        margin={20}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque auctor
+        consequat mauris. Vestibulum ornare vel odio ac hendrerit. Curabitur
+        lacinia sem id pharetra hendrerit. Nam quis lacus sit amet sapien
+        dapibus eleifend. Nunc quis augue nulla.Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Quisque auctor consequat mauris. Vestibulum
+        ornare vel odio ac hendrerit. Curabitur lacinia sem id pharetra
+        hendrerit. Nam quis lacus sit amet sapien dapibus eleifend. Nunc quis
+        augue nulla.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Quisque auctor consequat mauris. Vestibulum ornare vel odio ac
+        hendrerit. Curabitur lacinia sem id pharetra hendrerit. Nam quis lacus
+        sit amet sapien dapibus eleifend. Nunc quis augue nulla.
+      </Description>
     </div>
   );
 };
--- client/src/components/Descriptions/Description.tsx
+++ client/src/components/Descriptions/Description.tsx
@@ -1,32 +1,58 @@
-import React from "react";
-import { Typography, styled, Grid } from "@mui/material";
+import { Typography, styled, Grid, GridProps } from "@mui/material";
+import { theme } from "src/theme";
+import { Button } from "../";
+
+interface Props {
+  heading: string;
+  children: string;
+  Icon?: any;
+  button?: boolean;
+  margin?: number;
+}
+interface StyleProps extends GridProps {
+  marginValue: number;
+}
 
 const StyledHeading = styled(Typography)(({ theme }) => ({
-  color: theme.palette.primary.main,
   paddingBottom: theme.spacing(2),
   width: "100%",
 }));
 
-interface Props {
-  image?: any;
-  heading: string;
-  children: string;
-}
+const HeadingContainer = styled(Grid)((props: StyleProps) => {
+  const { marginValue } = props;
+  return {
+    color: theme.palette.primary.main,
+    justifyContent: "center",
+    marginTop: theme.spacing(marginValue | 0),
+    "& svg": {
+      fontSize: "4rem",
+      marginBottom: theme.spacing(1),
+    },
+  };
+});
+
+const ButtonContainer = styled("div")(({ theme }) => ({
+  margin: theme.spacing(3),
+}));
 
 const Description = (props: Props) => {
   //todo: handle image
-  const { heading, children, image } = props;
+  const { heading, children, Icon, button, margin = 0 } = props;
+  console.log(Icon, button);
   return (
     <Grid
       container
-      alignItems="center"
       sx={{
         color: "palette.primary",
+        justifyContent: "center",
       }}
     >
-      <StyledHeading textAlign="center" variant="h6">
-        {heading}
-      </StyledHeading>
+      <HeadingContainer container alignItems="center" marginValue={margin}>
+        {Icon && <Icon />}
+        <StyledHeading textAlign="center" variant="h6">
+          {heading}
+        </StyledHeading>
+      </HeadingContainer>
       <Typography
         component="p"
         textAlign="center"
@@ -35,6 +61,13 @@ const Description = (props: Props) => {
       >
         {children}
       </Typography>
+      {button && (
+        <ButtonContainer>
+          <Button variant="outlined" color="primary">
+            See Methodology
+          </Button>
+        </ButtonContainer>
+      )}
     </Grid>
   );
 };
