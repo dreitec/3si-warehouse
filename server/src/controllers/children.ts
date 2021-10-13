869771ab358801b203c7d3eff663727f28ae8c07
Steven Herzog <herzogdev@gmail.com>
2021-10-14 01:11:12 +0500

    remove dummy date

--- server/src/controllers/children.ts
+++ server/src/controllers/children.ts
@@ -55,7 +55,7 @@ export const getChildrenEligibility = async () => {
     throw new Error("Could not connect to database");
   }
   return new Promise((resolve, reject) => {
-    let currentDate = new Date("02-02-2021");
+    let currentDate = new Date();
     const currentYear: number = currentDate.getFullYear();
     const lastYear: number = currentDate.getFullYear() - 1;
     const month: number = currentDate.getMonth() + 1;
@@ -92,6 +92,72 @@ export const getChildrenEligibility = async () => {
 			count(CHILD_ID) as children 
 			from CHILDREN
 			${dateCondition}
+			group by month, LOAD_DT
+			order by LOAD_DT;`,
+      complete: (err, statement, rows) => {
+        console.log(statement, "statement");
+        if (err) {
+          throw new Error(err);
+        }
+        const total: number = rows.reduce(
+          (partial_sum, a) => partial_sum + a.CHILDREN,
+          0
+        );
+        const data = rows.map((elem) => ({
+          percentage: ((elem.CHILDREN / total) * 100).toFixed(2),
+          number: elem.CHILDREN,
+          group: `${months[elem.MONTH - 1]}, ${elem.YEAR}`,
+        }));
+        resolve({ data });
+      },
+    });
+  });
+};
+
+export const getChildrenServed = async () => {
+  const db = getDb();
+  if (!db) {
+    throw new Error("Could not connect to database");
+  }
+  return new Promise((resolve, reject) => {
+    let currentDate = new Date();
+    const currentYear: number = currentDate.getFullYear();
+    const lastYear: number = currentDate.getFullYear() - 1;
+    const month: number = currentDate.getMonth() + 1;
+    let dateCondition: string = ``;
+    console.log(month);
+    if (month < 6) {
+      dateCondition = `where (month >= ${
+        12 - (6 - month)
+      } AND year = ${lastYear}) OR ( month <= ${month} AND year = ${currentYear})`;
+    } else {
+      dateCondition = `where (month > ${
+        month - 6
+      })  AND ( year = ${currentYear})`;
+    }
+    console.log(dateCondition);
+    // let conditions = ``;
+    // const conditions = `            AND (
+    //             (AGE_GROUP IN ('Infant','Toddler','Preschool', 'School Age') AND INCOME_BRACKET like '%_399%' AND UNEMPLOYMENT = 0) OR
+    //             (AGE_GROUP like 'Preschool' ) OR
+    //             (AGE_GROUP IN ('Toddler','Preschool') ) OR
+    //             (AGE_GROUP like 'Preschool'  AND TOWNSHIP like 'BOSTON') OR
+    //             (AGE_GROUP like 'Preschool' AND TOWNSHIP like 'BOSTON') OR
+    //             (AGE_GROUP like 'Preschool' AND INCOME_BRACKET like '%under_100%') OR
+    //             (AGE_GROUP IN ('Infant','Toddler') AND INCOME_BRACKET like '%under_100%') OR
+    //             (AGE_GROUP like 'Preschool')
+    //         )	`
+
+    (db as any).execute({
+      sqlText: `
+			select  
+			DATE(LOAD_DT) as date, 
+			MONTH(date) as month, 
+			YEAR(date) as year,
+			count(CHILD_ID) as children 
+			from CHILDREN
+			${dateCondition}
+			AND PROGRAM_NAME not like 'Unserved'
 			group by month, LOAD_DT
 			order by LOAD_DT;`,
       complete: (err, statement, rows) => {
