6bc2932332ccf33149deb34324403dfa03fed539
Steven Herzog <herzogdev@gmail.com>
2021-11-04 06:43:01 +0500

    add loading button

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Button, ButtonProps, styled } from "@mui/material";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
+import { colors } from "../../theme/colors";
 
 const StyledPrimaryLightButton = styled(Button)(({ theme }) => ({
   color: theme.palette.primary.light,
@@ -8,12 +11,47 @@ const StyledPrimaryLightButton = styled(Button)(({ theme }) => ({
   paddingRight: ` ${theme.spacing(6)} !important`,
 }));
 
-const CustomButton = (props: ButtonProps) => {
-  const { children, ...otherProps } = props;
+interface IButtonProps {
+  loading?: boolean;
+  isLight?: boolean;
+}
+
+const CustomButton = (props: IButtonProps & ButtonProps) => {
+  const { children, loading = false, isLight = true, ...otherProps } = props;
+  if (isLight) {
+    return (
+      <StyledPrimaryLightButton {...otherProps}>
+        {children}
+      </StyledPrimaryLightButton>
+    );
+  }
+
   return (
-    <StyledPrimaryLightButton {...otherProps}>
-      {children}
-    </StyledPrimaryLightButton>
+    <Box
+      sx={{
+        m: 1,
+        position: "relative",
+        width: "fit-content",
+        display: "inline-block",
+      }}
+    >
+      <Button variant="contained" disabled={loading} {...otherProps}>
+        {children}
+      </Button>
+      {loading && (
+        <CircularProgress
+          size={24}
+          sx={{
+            color: colors.primary.main,
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            marginTop: "-12px",
+            marginLeft: "-12px",
+          }}
+        />
+      )}
+    </Box>
   );
 };
 
