01749947947af3a4e0dda9806399d453753bbb5b
Steven Herzog <herzogdev@gmail.com>
2021-11-19 21:02:59 +0500

    update env variable names

--- src/backend/utils/uploads3.ts
+++ src/backend/utils/uploads3.ts
@@ -3,8 +3,8 @@ const AWS = require("aws-sdk");
 const zlib = require("zlib");
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.NEXT_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.NEXT_AWS_SECRET_ACCESS_KEY,
   signatureVersion: "v4",
 });
 
